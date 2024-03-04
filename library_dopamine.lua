b69e309e3422df82eb8e5e9fa000e5b1cc7fa53e
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-03-04 03:27:10 -0800

    Update and rename library_dopamine_test.lua to library_dopamine.lua

--- library_dopamine.lua
+++ library_dopamine.lua
@@ -1581,7 +1581,7 @@ function library:init()
                         X = math.clamp((pos.X - objs.mainColor.Object.Position.X) / X, 0, 0.995)
                         Y = math.clamp((pos.Y - objs.mainColor.Object.Position.Y) / Y, 0, 0.995)
                         sat, val = 1 - X, 1 - Y;
-                        window.colorpicker.selected:SetColor(fromhsv(hue,1 - sat,val));
+                        window.colorpicker.selected:SetColor(fromhsv(hue,sat,val));
                         window.colorpicker:Visualize(fromhsv(hue, sat, val), window.colorpicker.selected.trans);
                     end
                 end
@@ -1592,8 +1592,8 @@ function library:init()
                         X = (objs.hue.Object.Position.X + objs.hue.Object.Size.X) - objs.hue.Object.Position.X
                         X = math.clamp((pos.X - objs.hue.Object.Position.X) / X, 0, 0.995)
                         hue = 1 - X
-                        window.colorpicker.selected:SetColor(fromhsv(hue,1 - sat,val));
-                        window.colorpicker:Visualize(fromhsv(hue, 1 - sat, val), window.colorpicker.selected.trans);
+                        window.colorpicker.selected:SetColor(fromhsv(hue,sat,val));
+                        window.colorpicker:Visualize(fromhsv(hue, sat, val), window.colorpicker.selected.trans);
                     end
                 end
 
