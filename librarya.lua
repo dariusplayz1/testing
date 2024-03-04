0aa84e68e33849624980ad892392152d366af741
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-03-03 17:25:20 -0800

    Update and rename library.lua to librarya.lua

--- librarya.lua
+++ librarya.lua
@@ -1593,7 +1593,7 @@ function library:init()
                         X = math.clamp((pos.X - objs.hue.Object.Position.X) / X, 0, 0.995)
                         hue = 1 - X
                         window.colorpicker.selected:SetColor(fromhsv(hue,1 - sat,val));
-                        window.colorpicker:Visualize(fromhsv(hue, sat, val), window.colorpicker.selected.trans);
+                        window.colorpicker:Visualize(fromhsv(hue, 1 - sat, val), window.colorpicker.selected.trans);
                     end
                 end
 
