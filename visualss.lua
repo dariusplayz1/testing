d55afd4a31be609eb40c2b0df7018bf4082c5934
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-04-12 06:44:26 -0700

    Update visualss.lua

--- visualss.lua
+++ visualss.lua
@@ -253,7 +253,8 @@ do -- Player Metatable
                 Image.Visible = false
                 return
             end
-            local Current_Health, Health_Maximum, Humanoid = ESP:Get_Health(self.Player), Humanoid.MaxHealth, Character:FindFirstChildOfClass("Humanoid")
+            local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+            local Current_Health, Health_Maximum = ESP:Get_Health(self.Player), Humanoid.MaxHealth
             if Head and HumanoidRootPart and Current_Health > 0 then
                 local Dimensions = Framework:Get_Bounding_Vectors(HumanoidRootPart)
                 local HRP_Position, On_Screen = Camera:WorldToViewportPoint(HumanoidRootPart.Position)
